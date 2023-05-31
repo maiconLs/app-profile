d54549c10e62647157427c654700f7701e5ae44a
Maicon <maiconlsilva10@gmail.com>
2023-05-31 19:35:01 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+import { Home } from './src/pages/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
+
+const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+};
+export default App;
